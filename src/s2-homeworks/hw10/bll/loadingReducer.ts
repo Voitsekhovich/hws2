dd4087f4234cb2520704335ef36026e3a37cbee1
Voitsekhovich <leshavoitsekhovich@gmail.com>
2025-01-04 15:10:30 +0300

    hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,18 @@
+
+
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionsType):{isLoading:boolean} => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+           return {...state,isLoading:action.isLoading}
+
+        }
+             
+        
 
         default:
             return state
@@ -20,3 +28,7 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+
+export type loadingActionType = ReturnType<typeof loadingAC>
+type ActionsType =loadingActionType
